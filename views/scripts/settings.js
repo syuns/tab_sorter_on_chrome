e6eb48c39d1843ec96207de8283bb50e09b2d326
syuns <syun1000@gmail.com>
2011-08-19 18:21:26 +0900

    urlのhostとpathを考慮したソート方法を追加

--- views/scripts/settings.js
+++ views/scripts/settings.js
@@ -1,37 +1,37 @@
 var settings = {
     get sortKey() {
-	return localStorage["sortKey"] || "url";
+	    return localStorage["sortKey"] || "hostAndPath";
     },
     set sortKey(value) {
-	localStorage["sortKey"] = value;
+	    localStorage["sortKey"] = value;
     },
     get removeSameTabs() {
-	return (localStorage["removeSameTabs"] || "") === "true";
+	    return (localStorage["removeSameTabs"] || "false") === "true";
     },
     set removeSameTabs(value) {
-	localStorage["removeSameTabs"] = value;
+	    localStorage["removeSameTabs"] = value;
     },
     get isAlways() {
-	return (localStorage["isAlways"] || "") === "true";
+	    return (localStorage["isAlways"] || "false") === "true";
     },
     set isAlways(value) {
-	localStorage["isAlways"] = value;
+	    localStorage["isAlways"] = value;
     },
     get isAscending() {
-	return (localStorage["isAscending"] || "") === "true";
+	    return (localStorage["isAscending"] || "false") === "true";
     },
     set isAscending(value) {
-	localStorage["isAscending"] = value;
+	    localStorage["isAscending"] = value;
     }
 };
 
 function load(value, id) {
     var options = document.getElementById(id);
     for (var i = 0; i < options.length; i++) {
-	if (options.children[i].value == value) {
-	    options.children[i].selected = "true";
-	    break;
-	}
+	    if (options.children[i].value == value) {
+	        options.children[i].selected = "true";
+	        break;
+	    }
     }
 }
 
--- views/scripts/sort.js
+++ views/scripts/sort.js
@@ -1,88 +1,132 @@
-function countPinnedTab(tabs) {
+/**
+ * @param String separator 
+ * @return String
+ */
+String.prototype.joinReversedText = function (separator) {
+    var array = this.split(separator);
+    array.reverse();
+    return array.join(separator);
+}
+
+/**
+ * Object : tabs
+ * return Number
+ */
+Object.prototype.countPinnedTab = function() {
     var counter = 0;
 
-    for (var i = 0, n = tabs.length; i < n; i++) {
-	if (tabs[i].pinned)
-	    counter++;
+    for (var i = 0, n = this.length; i < n; i++) {
+	    if (this[i].pinned)
+	        counter++;
     }
 
     return counter;
 }
 
-function sort(sortKey, isAscending) {
-    chrome.windows.getCurrent(function (window) {
-	chrome.tabs.getAllInWindow(window.id, function(tabs) {
-	    tabs.sort(function (a, b) {
-		if (isAscending)
-		    return a[sortKey] > b[sortKey] ? 1 : -1;
-		else
-		    return a[sortKey] > b[sortKey] ? -1 : 1;
-	    });
-
-	    var origin = countPinnedTab(tabs);
-	    for (var i = 0, n = tabs.length; i < n; i++) {
-		chrome.tabs.move(
-		    tabs[i].id,
-		    { windowId: window.id, index: origin + i }
-		);
+/**
+ * Object : tabs
+ */
+Object.prototype.setHostAndPath = function () {
+    if (settings.sortKey === "hostAndPath") {
+        var endString = "zzz://";
+	    for (var i = 0, n = this.length; i < n; i++) {
+            // $1 = protocol, $2 = host, $3 = port, $4 = path
+            var match = this[i].url.match("^(.+?)://(.+?):?(\d+)?(/.*)?$");
+            if (match && match[1] && match[2] && match[4]) {
+                if (match[1] == "chrome") {
+                    this[i]["hostAndPath"] = endString + this[i].url;
+                } else {
+                    this[i]["hostAndPath"] = match[2].joinReversedText(".") + match[4];
+                }
+            } else {
+                this[i]["hostAndPath"] = endString + this[i].url;
+            }
 	    }
-	});
-    });
+    }
 }
 
+/**
+ * Object : tabs
+ * @param String sortKey
+ * @param Boolean isAscending
+ */
+Object.prototype.sortTabs = function (sortKey, isAscending) {
+    this.sort(function (a, b) {
+        if (isAscending)
+            return a[sortKey] > b[sortKey] ? 1 : -1;
+        else
+            return a[sortKey] > b[sortKey] ? -1 : 1;
+    });
+
+    var origin = this.countPinnedTab();
+    for (var i = 0, n = this.length; i < n; i++) {
+        chrome.tabs.move(
+            this[i].id,
+            { windowId: window.id, index: origin + i }
+        );
+    }
+}
+
+/**
+ * @param String sortKey
+ * @param Boolean isAscending
+ * @param Function func(Boolean sorted, Object[] tabs)
+ */
 function isSorted(sortKey, isAscending, func) {
     chrome.windows.getCurrent(function (window) {
-	chrome.tabs.getAllInWindow(window.id, function(tabs) {
-	    var sorted = true;
-	    for (var i = countPinnedTab(tabs), n = tabs.length - 1; i < n && sorted; i++) {
-		if (isAscending)
-		    sorted = tabs[i][sortKey] <= tabs[i + 1][sortKey];
-		else
-		    sorted = tabs[i][sortKey] >= tabs[i + 1][sortKey];
-	    }
-	    func(sorted, tabs);
-	});
+        chrome.tabs.getAllInWindow(window.id, function(tabs) {
+            tabs.setHostAndPath();
+
+            var sorted = true;
+            for (var i = tabs.countPinnedTab(), n = tabs.length - 1; i < n && sorted; i++) {
+                if (isAscending)
+                    sorted = tabs[i][sortKey] <= tabs[i + 1][sortKey];
+                else
+                    sorted = tabs[i][sortKey] >= tabs[i + 1][sortKey];
+            }
+            func(sorted, tabs);
+        });
     });
 }
 
-chrome.browserAction.onClicked.addListener(function (tab) {  
+chrome.browserAction.onClicked.addListener(function (tab) {
     isSorted(
-	settings.sortKey,
-	settings.isAscending,
-	function (sorted, tabs) {
-	    if (sorted) {
-		var counter = 0;
-		if (settings.removeSameTabs) {
-		    var object = new Object();
-		    for (var i = 0, n = tabs.length; i < n; i++) {
-			var value = tabs[i][settings.sortKey];
-			if (!object[value])
-			    object[value] = 0;
+        settings.sortKey,
+        settings.isAscending,
+        function (sorted, tabs) {
+            if (sorted) {
+                var counter = 0;
+                if (settings.removeSameTabs) {
+                    var object = { };
+                    for (var i = 0, n = tabs.length; i < n; i++) {
+                        var value = tabs[i][settings.sortKey];
+                        if (!object[value])
+                            object[value] = 0;
 
-			if (++object[value] >= 2) {
-			    chrome.tabs.remove(tabs[i].id);
-			    counter++;
-			}
-		    }
-		}
-		if (counter > 0)
-		    return;
+                        if (++object[value] >= 2) {
+                            chrome.tabs.remove(tabs[i].id);
+                            counter++;
+                        }
+                    }
+                }
+                if (counter > 0)
+                    return;
 
-		settings.isAscending = !settings.isAscending;
-	    }
+                settings.isAscending = !settings.isAscending;
+            }
 
-	    sort(settings.sortKey, settings.isAscending);
-	}
+            tabs.sortTabs(settings.sortKey, settings.isAscending);
+        }
     );
 });
-    
+
 chrome.tabs.onUpdated.addListener(function (tabid, selectinfo, tab) {
     isSorted(
-	settings.sortKey,
-	settings.isAscending,
-	function (sorted) {
-	    if (!sorted && settings.isAlways)
-		sort(settings.sortKey, settings.isAscending);
-	}
+        settings.sortKey,
+        settings.isAscending,
+        function (sorted, tabs) {
+            if (!sorted && settings.isAlways)
+                tabs.sortTabs(settings.sortKey, settings.isAscending);
+        }
     );
 });
